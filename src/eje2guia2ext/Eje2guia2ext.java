c02db3feb1716eda87789b9065e6134b213445bd
natrix1 <141676108+natrix1@users.noreply.github.com>
2023-08-18 15:58:35 -0300

    Initial commit

---
+++ src/eje2guia2ext/Eje2guia2ext.java
@@ -0,0 +1,24 @@
+/*
+Ejercicio 2:
+Desarrolla una aplicación de gestión de contactos utilizando interfaces gráficas y colecciones en
+Java. La interfaz gráfica debe permitir al usuario agregar nuevos contactos, buscar contactos
+existentes, editar la información de un contacto y eliminar contactos de la lista. Cada contacto
+debe contener al menos los campos de nombre, número de teléfono y dirección de correo
+electrónico. Utiliza una colección apropiada para almacenar los contactos y asegúrate de que la
+interfaz gráfica refleje correctamente los cambios realizados en la lista de contactos.
+Es importante que repartan las tareas entre los miembros del equipo, de tal forma que: uno crea
+repositorio en GitHub, agrega a sus compañeros como colaboradores y luego los demás clonan,
+y cada uno hace una de las clases que forman parte del proyecto y suben al repositorio los
+cambios.
+ */
+package eje2guia2ext;
+
+
+public class Eje2guia2ext {
+
+   
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
